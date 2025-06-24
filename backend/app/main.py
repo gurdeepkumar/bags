27d6d67537e761bd167e40b8146f9bf091c651c9
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2025-06-24 22:22:56 +0100

    Security: CORS and HTTPS only.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import user, asset, portfolio
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+
 
 # Main FastAPI instance
 app = FastAPI(
     docs_url=None,
     redoc_url=None,
     openapi_url=None,
+    debug=False,
 )
 
+# CORS
+origins = [
+    "https://bags.gurdeepkumar.com",
+    "https://bags-api.gurdeepkumar.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# HTTPS
+app.add_middleware(HTTPSRedirectMiddleware)
 
 # Including the sub routes
 app.include_router(user.router)
