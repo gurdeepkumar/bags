5410e9ee4472c064058ee6d3e89b7966cf00692f
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2025-06-26 17:19:12 +0100

    Add: env settings.

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,23 +2,35 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import user, asset, portfolio
 from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DEBUG = os.getenv("DEBUG")
 
 
-# Main FastAPI instance
-app = FastAPI(
-    docs_url=None,
-    redoc_url=None,
-    openapi_url=None,
-    debug=False,
-)
+if DEBUG:
+    app = FastAPI()
+    origins = [
+        "https://bags.gurdeepkumar.com",
+        "https://bags-api.gurdeepkumar.com",
+        "https://localhost:3000",
+        "https://127.0.0.1:3000",
+    ]
+else:
+    app = FastAPI(
+        docs_url=None,
+        redoc_url=None,
+        openapi_url=None,
+        debug=False,
+    )
+    origins = [
+        "https://bags.gurdeepkumar.com",
+        "https://bags-api.gurdeepkumar.com",
+    ]
+    app.add_middleware(HTTPSRedirectMiddleware)
 
-# CORS
-origins = [
-    "https://bags.gurdeepkumar.com",
-    "https://bags-api.gurdeepkumar.com",
-    "http://localhost:3000",  # your React frontend URL
-    "http://127.0.0.1:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,9 +39,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# HTTPS
-# app.add_middleware(HTTPSRedirectMiddleware)
 
 # Including the sub routes
 app.include_router(user.router)
